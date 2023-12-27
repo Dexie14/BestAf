0602dc868058ada4b5f9077a763ef6b7de4358c8
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2023-12-27 07:37:54 -0800

    reset password

--- components/Auth/SavePass.jsx
+++ components/Auth/SavePass.jsx
@@ -2,7 +2,34 @@
 
 import React, { useState } from "react";
 
+import { useSearchParams } from "next/navigation";
+
+import { useResetPassword } from "@/hooks/auth/useResetPass";
+import ResetSuccess from "./resetSuccess";
+import ReactModal from "react-modal";
+const customStyles = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    opacity: "10",
+  },
+  content: {
+    height: "60vh",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    margin: "auto",
+    borderRadius: "30px",
+    width: "40%",
+  },
+};
+
 const SavePass = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordsMatch, setPasswordsMatch] = useState(true);
@@ -27,12 +54,29 @@ const SavePass = () => {
     }
   };
 
+  const searchParams = useSearchParams();
+  const url = `${searchParams}`;
+  // console.log(url)
+
+  const tok = url.split("=")[1]
+  // console.log(tok)
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const { mutate: resetting, isPending } = useResetPassword();
+
+  const createNewPassword = () => {
+    resetting({ tok, password, handleOpenModal });
+  };
+
   return (
     <div className="pt-10">
       <h3 className="mt-14 flex justify-center text-2xl font-medium">
         Enter your email to reset password
       </h3>
-      <form
+      <section
         className="bg-white rounded-3xl px-10 py-10 w-5/12 mx-auto my-10"
         style={{ boxShadow: "0px 2px 14px 0px rgba(51, 51, 51, 0.15);" }}
       >
@@ -74,11 +118,24 @@ const SavePass = () => {
         )}
 
         <div className="mb-10 mt-5">
-          <button className="bg-primary text-white flex justify-center items-center w-full rounded-lg px-3 py-3">
-            Save
+          <button
+            onClick={createNewPassword}
+            className="bg-primary text-white flex justify-center items-center w-full rounded-lg px-3 py-3"
+          >
+            {isPending ? "Saving" : "Save"}
           </button>
         </div>
-      </form>
+      </section>
+      <ReactModal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        ariaHideApp={false}
+        shouldCloseOnOverlayClick={true}
+        overlayClassName={"h-full left-0 bg-[#0000009b] z-[99999]"}
+        style={customStyles}
+      >
+        <ResetSuccess setModalIsOpen={setIsOpen} modalIsOpen={isOpen} />
+      </ReactModal>
     </div>
   );
 };
